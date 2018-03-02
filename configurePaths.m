170bb741d5901d2a93e19ca12689eafb894c3081
Gripnook <XxAndreixX@hotmail.com>
2018-03-02 09:31:45 -0500

    Improved path configuration
    - Added the option to specify the simulator's directory when configuring
    paths.

--- configurePaths.m
+++ configurePaths.m
@@ -1,12 +1,18 @@
-function configurePaths()
+function configurePaths(directory)
 %CONFIGUREPATHS  Configure the simulator's paths.
+%   CONFIGUREPATHS(DIRECTORY) configures the simulator's paths given the
+%   simulator's top level directory. DIRECTORY is an optional argument that
+%   defaults to the current directory.
 
-addpath(genpath('simulator'));
+if ~exist('directory', 'var')
+    directory = '.';
+end
+
+addpath(genpath([directory, filesep, 'simulator']));
 
 paths = getLibraryPaths();
 for i = 1:numel(paths)
-    addpath(genpath(paths{i}));
+    addpath(genpath([directory, filesep, paths{i}]));
 end
 
 end
-
