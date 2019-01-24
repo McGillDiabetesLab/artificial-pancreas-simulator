1ca5bcf4cb5018f395bcac0ab46f526cccf5109a
Anas El Fathi <anas.elfathi@mail.mcgill.ca>
2019-01-24 09:37:06 -0500

    added default config file

--- simulator/GUI/getConfigurationFilename.m
+++ simulator/GUI/getConfigurationFilename.m
@@ -3,5 +3,11 @@ function filename = getConfigurationFilename()
 
 filename = '.config.mat';
 
+% Copy default config file if no configuration.
+if exist(filename, 'file') ~= 2
+    filepath = fileparts(which('default.config.mat'));
+    copyfile(which('default.config.mat'), [filepath, '\..\', filename])
+end
+
 end
 
