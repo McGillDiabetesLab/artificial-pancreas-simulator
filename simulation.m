a2c5bba8243aae8aa344489ec57f05dd0fbe34cb
El Fathi Anas <anas.elfathi@mail.mcgill.ca>
2020-06-25 19:14:46 -0400

    better examples for runing simulation with gui or as script

--- simulation.m
+++ simulation.m
@@ -1,11 +1,28 @@
-%SIMULATION  Start the artificial pancreas simulator GUI.
+%SIMULATION  Run the artificial pancreas simulator without GUI.
+clc,
+clear,
 
-%% Keep the simulator in the global namespace.
-global simulator;
-
-%% Configure the simulator's paths.
 configurePaths();
 
-%% Start the GUI.
-GUISimulator();
+options = SimulatorOptions;
+options.simulationDuration = 24 * 60; % minutes
+options.simulationStartTime = 8 * 60; % minutes
+options.simulationStepSize = 10; % minutes
+options.parallelExecution = false;
+options.resultsManager = 'PublishResultsManager';
 
+optPatient = options.getOptions('HovorkaPatient');
+optPatient.sensorNoiseType = 'ar(1)';
+optPatient.intraVariability = 0.6;
+optPatient.mealVariability = 0.4;
+optPatient.RNGSeed = 10;
+
+% Set virtual patient options
+options.virtualPatients{1} = { ...
+    {'HovorkaPatient', optPatient}, ...
+    'DailyMealPlan', ...
+    'EmptyExercisePlan', ...
+    'PumpTherapy'};
+
+simulator = ArtificialPancreasSimulator(options);
+simulator.simulate();
